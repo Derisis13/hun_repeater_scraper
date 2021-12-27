b765b4231848c0dd4efc6424dfe63fddc725d2d4
Derisis13 <email.felhasznalasra@gmail.com>
2021-12-27 23:33:28 +0100

    Licensing under Apache Licene 2.0

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python
+
+# Copyright [2021] [László Párkányi]
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import pandas as pd
 
 twometer_call = {"2M CALL", "145.500000", "", "0.000000", "", "88.5", "88.5", "023", "NN", "FM", "5.00", "",
